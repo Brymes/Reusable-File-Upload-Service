511dbbc2711a0e5b2d22048f82e426f0b7da3b05
Timothy Odebunmi <dev.brymes@gmail.com>
2022-07-16 22:49:17 +0100

    Data ase Model

---
+++ models/uploads.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"Upload-Service/config"
+	"Upload-Service/utils"
+	"context"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"log"
+)
+
+type Upload struct {
+	ID primitive.ObjectID `bson:"_id,  omitempty" json:"-"`
+	// Implement
+	Identifier string   `bson:"postID" json:"postID" binding:"required"`
+	UploadURL  string   `bson:"upload_url" json:"upload_url"`
+	Tags       []string `bson:"tags" json:"tags"`
+}
+
+func (u Upload) SaveUpload(logger *log.Logger) {
+	//TODO Implement uniqueness for  Field Identifier
+
+	ctx := context.TODO()
+	_, err := config.MongoClient.Collection("Uploads").InsertOne(ctx, u)
+	utils.LogErr(err, logger)
+
+	return
+}
+
+func (u Upload) DeleteUpload() {
+
+}
