165203966526a2b6dd5da0ca5b0e91bca7b54bc3
Timothy Odebunmi <dev.brymes@gmail.com>
2022-07-18 10:48:14 +0100

    Cmpleted Cloudinary

---
+++ services/cloudinary.go
@@ -0,0 +1,41 @@
+package services
+
+import (
+	"Upload-Service/config"
+	"Upload-Service/utils"
+	"context"
+	"fmt"
+	"github.com/cloudinary/cloudinary-go/v2/api"
+	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
+	"log"
+	"os"
+)
+
+type Cloudinary struct{}
+
+func (c Cloudinary) Upload(s ServicePayload, logger *log.Logger) (url, publicID string) {
+	ctx := context.Background()
+
+	resp, err := config.CloudinaryInstance.Upload.Upload(ctx, s.Filepath, uploader.UploadParams{Tags: s.Tags})
+	utils.LogErr(err, logger)
+
+	url, publicID = resp.SecureURL, resp.PublicID
+
+	return
+}
+
+func (c Cloudinary) SignUpload(s ServicePayload, notificationURL string, logger *log.Logger) string {
+	//NOTE
+	// Append parameters to the form data. The parameters that are signed using
+	// the signing function (signuploadform) need to match these.
+	paramsToSign, err := api.StructToParams(uploader.UploadParams{Tags: s.Tags, NotificationURL: notificationURL})
+	utils.LogErr(err, logger)
+
+	resp, err := api.SignParameters(paramsToSign, os.Getenv("CLOUDINARY_API_SECRET"))
+	utils.LogErr(err, logger)
+
+	logger.Println(resp)
+
+	return fmt.Sprintf("https://api.cloudinary.com/v1_1/%v/auto/upload", resp)
+
+}
