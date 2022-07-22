c0c73eb2e37719c252064032a1e3debef648d493
Timothy Odebunmi <dev.brymes@gmail.com>
2022-07-22 12:08:39 +0100

    Initialize S3 & show if service active

--- config/initServices.go
+++ config/initServices.go
@@ -1,18 +1,78 @@
 package config
 
 import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/cloudinary/cloudinary-go/v2"
 	"log"
+	"os"
 )
 
-var CloudinaryInstance *cloudinary.Cloudinary
+var (
+	CloudinaryInstance *CloudinaryClient
+	DefaultBucket      string
+	S3Instance         *S3Client
+)
+
+type S3Client struct {
+	Client   *s3.Client
+	Bucket   string
+	IsActive bool
+}
+
+type CloudinaryClient struct {
+	Client   *cloudinary.Cloudinary
+	IsActive bool
+}
 
 func InitCloudinary() {
 	var err error
+	CloudinaryInstance.IsActive = true
 
-	CloudinaryInstance, err = cloudinary.New()
+	if os.Getenv("CLOUDINARY_API_SECRET") == "" {
+		log.Println("Failed to initialize Cloudinary" + err.Error())
+		CloudinaryInstance.IsActive = false
+	}
+
+	CloudinaryInstance.Client, err = cloudinary.New()
 
 	if err != nil {
-		log.Fatalf("Failed to intialize Cloudinary, %v", err)
+		log.Println("Failed to initialize Cloudinary" + err.Error())
+		CloudinaryInstance.IsActive = false
 	}
+}
+
+func InitS3() {
+	S3Instance.IsActive = true
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+
+	if err != nil {
+		log.Println("configuration error, " + err.Error())
+		S3Instance.IsActive = false
+	}
+
+	S3Instance.Client = s3.NewFromConfig(cfg)
+	err = InitBucketS3()
+
+	if err != nil {
+		S3Instance.IsActive = false
+	}
+
+}
+
+func InitBucketS3() error {
+	bucket := os.Getenv("S3_DEFAULT_BUCKET")
+	if bucket == "" {
+		bucket = "Uploads"
+	}
+
+	input := &s3.CreateBucketInput{
+		Bucket: &bucket,
+	}
+
+	_, err := S3Instance.Client.CreateBucket(context.TODO(), input)
+
+	return err
 }
