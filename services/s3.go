f1221069db7d464938a8efb834ae93d507998f5b
Timothy Odebunmi <dev.brymes@gmail.com>
2022-07-27 13:09:42 +0100

    Completed S3 Integration

---
+++ services/s3.go
@@ -0,0 +1,83 @@
+package services
+
+import (
+	"Upload-Service/config"
+	"Upload-Service/utils"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type S3 struct{}
+
+func (s3Payload S3) Upload(s ServicePayload, logger *log.Logger) (url, publicID, message string) {
+	if config.S3Instance.IsActive != true {
+		return "", "", "Service Not Activated, Kindly Set Auth Keys"
+	}
+
+	file, err := os.Open(s.Filepath)
+
+	if err != nil {
+		logger.Panicf("Unable to Open file %v for upload", s.Filepath)
+	}
+
+	defer file.Close()
+
+	publicID, _ = utils.GenerateUniqueID(13)
+	if publicID == "" {
+		return "", "", "Internal Server Error Contact Support"
+	}
+
+	tags := utils.ConvertTagsToString(s.Tags)
+	input := &s3.PutObjectInput{
+		Bucket:  &config.S3Instance.DefaultBucket,
+		Key:     &publicID,
+		Body:    file,
+		Tagging: &tags,
+	}
+
+	if s.Bucket != "" {
+		input.Bucket = &s.Bucket
+	}
+	_, err = config.S3Instance.Client.PutObject(context.TODO(), input)
+
+	if err != nil {
+		return "", "", fmt.Sprintf("Internal Server Error: %v", err.Error())
+	}
+	url = ConstructObjectURL(publicID)
+
+	return
+}
+
+func (s3Payload S3) SignUpload(s ServicePayload, notificationURL string, logger *log.Logger) (signedURL, error string) {
+	if config.S3Instance.IsActive != true {
+		return "", "Service Not Activated, Kindly Set Auth Keys"
+	}
+	id, _ := utils.GenerateUniqueID(13)
+	if id == "" {
+		return "", "Internal Server Error Contact Support"
+	}
+
+	input := &s3.GetObjectInput{
+		Bucket: &config.S3Instance.DefaultBucket,
+		Key:    &id,
+	}
+
+	api := s3.NewPresignClient(config.S3Instance.Client)
+	resp, err := api.PresignGetObject(context.TODO(), input)
+
+	if err != nil || resp.URL == "" {
+		return "", "Internal Server Error Contact Support"
+	}
+
+	return resp.URL, "Signed URL generated successfully"
+}
+
+func ConstructObjectURL(objectKey string) (url string) {
+	s3Instance := config.S3Instance
+	url = fmt.Sprintf("https://%v.amazonaws.com/%v/%v", s3Instance.Region, s3Instance.DefaultBucket, objectKey)
+	return
+}
